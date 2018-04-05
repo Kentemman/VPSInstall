9505a9ad4335e18a6c99f7b873efcb5871984090
Kentemman <38069509+Kentemman@users.noreply.github.com>
2018-04-05 15:56:42 +0800

    Add files via upload

---
+++ theme/KEDLBlue.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/vps/public_html/asset > /dev/null 2>&1 &
+rm -rf /home/vps/public_html/view > /dev/null 2>&1 &
+rm -rf /home/vps/public_html/tmp/* > /dev/null 2>&1 &
+echo "Installing Blue Theme"
+cd ~ > /dev/null 2>&1 &
+mkdir KEDL > /dev/null 2>&1 &
+cd KEDL
+apt-get -y install zip unzip
+wget http://207.148.127.35:85/api/theme/BlueTheme.zip
+unzip BlueTheme.zip
+rm -f BlueTheme.zip
+mv asset /home/vps/public_html > /dev/null 2>&1 &
+mv view /home/vps/public_html > /dev/null 2>&1 &
+echo "Enjoy using Blue Theme -KEDL"
+
---
+++ theme/KEDLGreen.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Removing Old Theme"
+rm -rf /home/vps/public_html/asset > /dev/null 2>&1 &
+rm -rf /home/vps/public_html/view > /dev/null 2>&1 &
+rm -rf /home/vps/public_html/tmp/* > /dev/null 2>&1 &
+echo "Installing Blue Theme"
+cd ~ > /dev/null 2>&1 &
+mkdir KEDL > /dev/null 2>&1 &
+cd KEDL
+apt-get -y install zip unzip
+wget http://207.148.127.35:85/api/theme/GreenTheme.zip
+unzip GreenTheme.zip
+rm -f GreenTheme.zip
+mv asset /home/vps/public_html > /dev/null 2>&1 &
+mv view /home/vps/public_html > /dev/null 2>&1 &
+echo "Enjoy using Blue Theme -KEDL"
+
