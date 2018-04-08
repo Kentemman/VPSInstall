7382ef6ede366629e5c2548d5ed330ebcfc329de
Kentemman <38069509+Kentemman@users.noreply.github.com>
2018-04-08 18:11:33 +0800

    Update KEDLsao.sh
    wget https://raw.githubusercontent.com/Kentemman/api/master/theme/KEDLsao.sh && chmod +x KEDLsao.sh && ./KEDLsao.sh

--- theme/KEDLsao.sh
+++ theme/KEDLsao.sh
@@ -8,10 +8,9 @@ cd ~ > /dev/null 2>&1 &
 mkdir KEDL > /dev/null 2>&1 &
 cd KEDL
 apt-get -y install zip unzip
-wget https://raw.githubusercontent.com/Kentemman/api/master/theme/sao.zip
+wget https://www.dropbox.com/s/83ltb21v9o9njwn/sao.zip
 unzip sao.zip
 rm -f sao.zip
 mv asset /home/vps/public_html > /dev/null 2>&1 &
 mv view /home/vps/public_html > /dev/null 2>&1 &
 echo "Enjoy using My Theme -KEDL Theme Maker"
-
